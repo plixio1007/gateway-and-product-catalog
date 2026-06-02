fc89fa2ed9c36826995c26c334467c5f59ec2eb8
plixio1007 <raflyadie1407@gmail.com>
2026-06-03 00:19:57 +0700

    feat: update health check timeout and add class-transformer and class-validator dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,7 +7,7 @@ import databaseConfig from '@config/database.config';
 import jwtConfig from '@config/jwt.config';
 import minioConfig from '@config/minio.config';
 import { envValidationSchema } from '@config/env.validation';
-import { PrismaModule} from './prisma/prisma.modules';
+import { PrismaModule } from './prisma/prisma.modules';
 import { HealthModule } from './health/health.module';
 
 @Module({
